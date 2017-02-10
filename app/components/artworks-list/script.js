ee173d109d55588d9232736c9b45578653e376eb
Victor Polkó <Viktor.Polko@dataart.com>
2017-02-10 14:22:01 +0300

    Update artworks-list

--- app/components/artworks-list/script.js
+++ app/components/artworks-list/script.js
@@ -7,23 +7,21 @@ export default {
     }
   },
 
-  beforeCreate() {
-    // Get all data
-    // db.ref('/artworks').on('value', (snapshot) => {
-    //   this.artworks = snapshot.val();
-    // });
-
-    // Get all data and query it
-    db.ref('/artworks').orderByChild('published').on('value', (snapshot) => {
-      this.artworks = [];
-
-      snapshot.forEach((childSnapshot) => {
-        this.artworks.push(childSnapshot.val());
-      });
-    });
+  created() {
+    this.fetchData();
   },
 
   methods: {
+    fetchData() {
+      db.ref('/artworks').orderByChild('published').on('value', (snapshot) => {
+        this.artworks = [];
+
+        snapshot.forEach((childSnapshot) => {
+          this.artworks.unshift(childSnapshot.val());
+        });
+      });
+    },
+
     togglePublished(artwork) {
       db.ref(`artworks/${ artwork.id }/published`).set(!artwork.published);
     },
