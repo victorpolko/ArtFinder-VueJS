9ead2b9d05b1a53654113992a2b8ff5b5409f792
Victor Polkó <Viktor.Polko@dataart.com>
2017-02-13 03:32:08 +0300

    Add publishing star svgs

--- app/components/artwork/index.vue
+++ app/components/artwork/index.vue
@@ -10,17 +10,21 @@
           span(v-show="artwork.title") {{ artwork.title }} by {{ artwork.artist }}
 
     .uk-align-center.uk-width-1-2(uk-grid)
-      div
-        .uk-card.uk-card-default
-          .uk-card-media-top
-            img(v-bind:src="artwork.image")
-            .uk-card-body
-              h3.uk-card-title {{ artwork.title }}
-              p {{ artwork.mediums }}
-              p {{ artwork.artist }}, {{ artwork.year }}
-              p
-                a(href="javascript:void(0)" v-on:click="togglePublished(artwork)") {{ status(artwork) }}
+      .uk-card.uk-card-default
+        .uk-card-media-top
+          img(v-bind:src="artwork.image")
+          .uk-card-body
+            h3.uk-card-title
+              | {{ artwork.title }}
+              svg.star.uk-align-right(v-bind:class="{ published: artwork.published }" v-on:click="togglePublished()")
+                use(xlink:href="#star")
+                  title {{ artwork.published ? 'Unpublish me' : 'Publish me' }}
+            p {{ artwork.mediums }}
+            p {{ artwork.artist }}, {{ artwork.year }}
 
+    svg.uk-hidden(xmlns="http://www.w3.org/2000/svg")
+      symbol(id="star" viewBox="0 0 20 20")
+        polygon(stroke-width="1.01" points="10 2 12.63 7.27 18.5 8.12 14.25 12.22 15.25 18 10 15.27 4.75 18 5.75 12.22 1.5 8.12 7.37 7.27")
 </template>
 
 <script type="text/javascript" src="./script.js"></script>
--- app/components/artworks-list/index.vue
+++ app/components/artworks-list/index.vue
@@ -38,8 +38,10 @@
               td {{ artwork.mediums }}
               td {{ artwork.artist }}
               td {{ artwork.year }}
-              td.uk-text-center
-                input.uk-checkbox(type="checkbox" v-bind:checked="artwork.published" v-on:change="togglePublished(artwork)")
+              td.uk-text-center.published(v-on:click="togglePublished(artwork)")
+                svg.star(v-bind:class="{ published: artwork.published }")
+                  use(xlink:href="#star")
+                    title {{ artwork.published ? 'Unpublish me' : 'Publish me' }}
 
       div(v-else)
         img.uk-align-center.uk-width-1-4(src="/empty.png")
@@ -75,12 +77,19 @@
               td {{ artwork.mediums }}
               td {{ artwork.artist }}
               td {{ artwork.year }}
-              td.uk-text-center
-                input.uk-checkbox(type="checkbox" v-bind:checked="artwork.published" v-on:change="togglePublished(artwork)")
+              td.uk-text-center.published(v-on:click="togglePublished(artwork)")
+                svg.star(v-bind:class="{ published: artwork.published }")
+                  use(xlink:href="#star")
+                    title {{ artwork.published ? 'Unpublish me' : 'Publish me' }}
+
 
       div(v-else)
         img.uk-align-center.uk-width-1-4(src="/empty.png")
+
     hr
+    svg.uk-hidden(xmlns="http://www.w3.org/2000/svg")
+      symbol(id="star" viewBox="0 0 20 20")
+        polygon(stroke-width="1.01" points="10 2 12.63 7.27 18.5 8.12 14.25 12.22 15.25 18 10 15.27 4.75 18 5.75 12.22 1.5 8.12 7.37 7.27")
 
 </template>
 
